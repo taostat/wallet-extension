92bf17c97fe91f5f79424d1c7fdd2fccaf57be5c
leemcmullen <lee@mcmullen.im>
2026-01-26 16:52:42 +0000

    remove unused script

--- packages/extension-shared/src/constants.ts
+++ packages/extension-shared/src/constants.ts
@@ -4,26 +4,21 @@ export const PORT_EXTENSION = `taostats-extension${PORT_SUFFIX}`
 export const PORT_CONTENT = `taostats-content${PORT_SUFFIX}`
 export const DEBUG = process.env.DEBUG === "true"
 export const TEST = process.env.NODE_ENV === "test"
-export const DEFAULT_ETH_CHAIN_ID = 1 //Ethereum mainnet
 
 export const IS_FIREFOX = process.env.BROWSER === "firefox"
 
 export const IPFS_GATEWAY = "https://talisman.mypinata.cloud/ipfs/"
 
-export const BLOCKAID_API_URL = "https://bap.talisman.xyz"
-export const TAOSTATS_BASE_PATH = process.env.TAOSTATS_BASE_PATH || "https://tsp.talisman.xyz"
-export const RAMPS_COINBASE_API_BASE_PATH = "https://coinbase-api.talisman.xyz"
-export const RAMPS_COINBASE_PAY_URL = "https://pay.coinbase.com"
-export const RAMPS_RAMP_API_URL = "https://ramp-api.talisman.xyz"
-export const ASSET_DISCOVERY_API_URL = "https://ada.talisman.xyz"
+export const TAOSTATS_BASE_PATH = process.env.TAOSTATS_BASE_PATH
+export const ASSET_DISCOVERY_API_URL = ""
 
 export const TAOSTATS_WEB_APP_DOMAIN = "taostats.io"
 export const TAOSTATS_WEB_APP_URL = "https://taostats.io"
 export const TAOSTATS_WEB_APP_STAKING_URL = "https://dash.taostats.io/stake"
 export const TAOSTATS_WEB_APP_SWAP_URL = "https://taostats.io/swap"
 
-export const SIGNET_LANDING_URL = "https://talisman.xyz/signet"
-export const SIGNET_APP_URL = "https://signet.talisman.xyz"
+export const SIGNET_LANDING_URL = ""
+export const SIGNET_APP_URL = ""
 
 // Wallet-specific invite link
 export const DISCORD_URL = "https://discord.gg/PVBxmwcyw5"
@@ -31,16 +26,10 @@ export const DISCORD_URL = "https://discord.gg/PVBxmwcyw5"
 // Docs URLs
 export const DOCS_URL_PREFIX = "https://docs.talisman.xyz/talisman"
 export const POLKADOT_VAULT_DOCS_URL = `${DOCS_URL_PREFIX}/start/importing-external-wallets/import-from-polkadot-vault`
-export const RELEASE_NOTES_URL = `${DOCS_URL_PREFIX}/about/wallet-release-notes`
-export const SECURITY_DOCS_URL = `${DOCS_URL_PREFIX}/about/security`
 export const PRIVACY_POLICY_URL = `${TAOSTATS_WEB_APP_URL}/extension/privacy-policy`
 export const TERMS_OF_USE_URL = `${TAOSTATS_WEB_APP_URL}/extension/terms-of-use`
-export const BRAVE_BALANCES_URL = `${DOCS_URL_PREFIX}/help-and-support/troubleshooting/balances-on-brave-not-showing`
-export const TOKEN_APPROVALS_URL = `${DOCS_URL_PREFIX}/navigate/using-talisman-with-a-website-dapp/token-approvals`
 export const CONNECT_LEDGER_DOCS_URL = `${DOCS_URL_PREFIX}/start/importing-external-wallets/import-from-ledger`
 
 // Images
 export const UNKNOWN_TOKEN_URL = "/images/unknown-token.svg"
 export const UNKNOWN_NETWORK_URL = "/images/unknown-network.svg"
-
-export const LEDGER_ETHEREUM_MIN_VERSION = "1.9.19"
