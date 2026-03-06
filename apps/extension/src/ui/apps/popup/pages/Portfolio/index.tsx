6c415757478798ed6328576294370db111b05956
leemcmullen <lee@mcmullen.im>
2026-03-06 11:59:46 +0000

    add hide balances toggle to popup ui

--- apps/extension/src/ui/apps/popup/pages/Portfolio/index.tsx
+++ apps/extension/src/ui/apps/popup/pages/Portfolio/index.tsx
@@ -1,6 +1,9 @@
+import { EyeIcon, EyeOffIcon } from "@taostats-wallet/icons"
 import { classNames } from "@taostats-wallet/util"
 import { FC, PropsWithChildren, Suspense, useEffect, useRef } from "react"
+import { useTranslation } from "react-i18next"
 import { Route, Routes, useLocation } from "react-router-dom"
+import { Tooltip, TooltipContent, TooltipTrigger } from "taostats-ui"
 
 import { ScrollContainer } from "@taostats/components/ScrollContainer"
 import { SuspenseTracker } from "@taostats/components/SuspenseTracker"
@@ -8,12 +11,68 @@ import { TaostatsLogo } from "@taostats/theme/logos"
 import { PortfolioContainer } from "@ui/domains/Portfolio/PortfolioContainer"
 // import BraveWarningPopupBanner from "@ui/domains/Settings/BraveWarning/BraveWarningPopupBanner"
 import MigratePasswordAlert from "@ui/domains/Settings/MigratePasswordAlert"
+import { useSetting } from "@ui/state"
 
 import { BottomNav } from "../../components/Navigation/BottomNav"
 import { NavigationDrawer } from "../../components/Navigation/NavigationDrawer"
 import { PortfolioAccounts } from "./PortfolioAccounts"
 import { PortfolioAsset } from "./PortfolioAsset"
 import { PortfolioAssets } from "./PortfolioAssets"
+
+const HideBalancesToggle = () => {
+  const { t } = useTranslation()
+  const [hideBalances, setHideBalances] = useSetting("hideBalances")
+
+  return (
+    <Tooltip placement="bottom-end">
+      <TooltipTrigger asChild>
+        <label
+          htmlFor="showBalancesNav"
+          className="relative inline-flex cursor-pointer items-center"
+        >
+          <input
+            id="showBalancesNav"
+            type="checkbox"
+            className="peer sr-only"
+            defaultChecked={!hideBalances}
+            onChange={(e) => setHideBalances(!e.target.checked)}
+          />
+          <div
+            className={classNames(
+              "bg-grey-600 peer h-14 w-28 shrink-0 rounded-full",
+              "peer-focus-visible:ring-body peer-focus:outline-none peer-focus-visible:ring-2",
+            )}
+          ></div>
+          <div
+            className={classNames(
+              "absolute left-1 top-1 flex h-12 w-12",
+              "bg-grey-800 rounded-full",
+              "peer-checked:bg-primary transition peer-checked:translate-x-14",
+            )}
+          >
+            <EyeIcon
+              className={classNames(
+                "absolute left-2 top-2 h-8 w-8",
+                "text-body-black transition-opacity",
+                hideBalances ? "opacity-0" : "opacity-100",
+              )}
+            />
+            <EyeOffIcon
+              className={classNames(
+                "absolute left-2 top-2 h-8 w-8",
+                "text-body transition-opacity",
+                !hideBalances ? "opacity-0" : "opacity-100",
+              )}
+            />
+          </div>
+        </label>
+      </TooltipTrigger>
+      <TooltipContent>
+        {hideBalances ? t("Balances: hidden") : t("Balances: visible")}
+      </TooltipContent>
+    </Tooltip>
+  )
+}
 
 const PortfolioRoutes = () => (
   <>
@@ -52,6 +111,7 @@ export const Portfolio = () => (
         <div className="flex size-full flex-col gap-4 py-8">
           <header className="flex items-center justify-between p-4 pt-0">
             <TaostatsLogo className="h-[1.5rem] w-auto" />
+            <HideBalancesToggle />
           </header>
           <PortfolioRoutes />
           <BottomNav />
