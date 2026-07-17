898b3d640460f9a3bc6788565f11f7adeca13f1e
leemcmullen <lee@mcmullen.im>
2026-07-17 12:12:33 +0100

    fix toast bg

--- apps/extension/src/@taostats/components/Notifications/NotificationsContainer.tsx
+++ apps/extension/src/@taostats/components/Notifications/NotificationsContainer.tsx
@@ -4,7 +4,7 @@ import { ToastContainer } from "react-toastify"
 
 export const NotificationsContainer = () => (
   <ToastContainer
-    toastClassName="!bg-secondary !rounded-md w-[330px] right-6 !font-sans mr-0 mb-xs border border-primary"
+    toastClassName="!bg-secondary-solid !rounded-md w-[330px] right-6 !font-sans mr-0 mb-xs border border-primary"
     bodyClassName="!px-2 !py-0"
     className="!left-auto !right-0 !top-6 !w-auto"
     progressClassName={"helloprogress"}
