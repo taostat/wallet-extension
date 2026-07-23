8cf4966435c5fe186314ab8e214fb63f2318582f
leemcmullen <lee@mcmullen.im>
2026-07-23 16:28:47 +0100

    fix lint error with slashes separator

--- apps/extension/src/ui/domains/Asset/BalanceSeparator.tsx
+++ apps/extension/src/ui/domains/Asset/BalanceSeparator.tsx
@@ -3,6 +3,6 @@ import { FC } from "react"
 
 export const BalanceSeparator: FC<{ className?: string }> = ({ className }) => (
   <span aria-hidden className={classNames("text-fg-brand text-[0.9em]", className)}>
-    //
+    {"//"}
   </span>
 )
